d32d50487507a14211a88a370420391774d1a6bb
Ankit Sahay <139492522+thebrokenapp@users.noreply.github.com>
2024-07-07 18:26:06 +0530

    Create app.py

---
+++ rest-api-from-scratch/code/step1-status/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/apiStatus')
+def status():
+	return {"message": "API is running!"}
+
+
+if __name__ == "__main__":
+	app.run(host="127.0.0.1", port= 5000, debug=True)
