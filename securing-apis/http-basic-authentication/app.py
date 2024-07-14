adf1179b2bd1113f0b5702fefe9c2c36d39b7486
Ankit Sahay <139492522+thebrokenapp@users.noreply.github.com>
2024-07-14 21:35:45 +0530

    Create app.py

---
+++ securing-apis/http-basic-authentication/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, {}!".format(auth.current_user())
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port= 5000)
