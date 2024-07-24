fa967369bfa39688e5e00e94fbb6d11f085883d8
Ankit Sahay <139492522+thebrokenapp@users.noreply.github.com>
2024-07-24 20:09:43 +0530

    Update app.py

--- securing-apis/http-basic-authentication/app.py
+++ securing-apis/http-basic-authentication/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -6,8 +6,8 @@ app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 users = {
-    "john": generate_password_hash("hello"),
-    "susan": generate_password_hash("bye")
+    "ankit": generate_password_hash("my_password"),
+    "kumar": generate_password_hash("12345")
 }
 
 @auth.verify_password
@@ -15,10 +15,22 @@ def verify_password(username, password):
     if username in users and check_password_hash(users.get(username), password):
         return username
 
-@app.route('/')
+@app.route('/apiStatus')
 @auth.login_required
 def index():
-    return "Hello, {}!".format(auth.current_user())
+    print(request.headers)
+    return {"message": "API is up!"}
+    #return "Hello, {}! API is up!".format(auth.current_user())
+
+@app.route('/noAuth')
+def noAuth():
+    return {"message": "API is up!"}
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port= 5000)
+
+# Try in POSTMAN
+# 1. Try requesting in a regualar way: you will get error
+# 2. Go to "Authorization Tab" and add username password
+# Note: Dont add username password in header. That work is done by POSTMAN
