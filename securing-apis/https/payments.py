3c1a05af6647f24e50d1d51e67a21fce38c97616
Ankit Sahay <139492522+thebrokenapp@users.noreply.github.com>
2024-11-21 21:21:00 +0530

    Update payments.py

--- securing-apis/https/payments.py
+++ securing-apis/https/payments.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify, Response
+from prometheus_flask_exporter import PrometheusMetrics
 from datetime import datetime
 import uuid
 from pydantic import BaseModel, Field, UUID4
 from flask_pydantic import validate
-from flask_cors import CORS
+import random
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Payment(BaseModel):
 	amount : int
@@ -23,9 +27,20 @@ class SignUp(BaseModel):
 		extra = "forbid"
 
 app = Flask(__name__)
-#CORS(app)
+auth = HTTPBasicAuth()
+#PrometheusMetrics(app)
+
+
+users_database = {}
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users_database and check_password_hash(users_database.get(username), password):
+        return username
 
 @app.route('/apiStatus')
+@auth.login_required
 def status():
 	return {"message": "API is up!"}
 
@@ -36,6 +51,7 @@ my_users = []
 
 
 @app.route('/payments', methods=["GET"])
+@auth.login_required
 def getPayments():
 	return {"data": payments}
 
@@ -43,6 +59,7 @@ def getPayments():
 
 @app.route('/payments', methods=["POST"])
 @validate()
+@auth.login_required
 def initiatePayment(body: Payment):
 	data = body.dict()				# extract the request body and store it in variable "data"
 	txn_id = str(uuid.uuid4())		# create a new transaction ID using uuid() library
@@ -58,6 +75,7 @@ def initiatePayment(body: Payment):
 
 @app.route('/payments/<transaction_id>')
 @validate()
+@auth.login_required
 def getPayment(transaction_id: UUID4):
 	for payment in payments:
 		if payment["transaction_id"] == str(transaction_id):
@@ -68,6 +86,7 @@ def getPayment(transaction_id: UUID4):
 
 
 @app.route('/payments/note/<note>')
+@auth.login_required
 def getPaymentByNote(note):
 	for payment in payments:
 		if payment["note"] == note:
@@ -77,6 +96,7 @@ def getPaymentByNote(note):
 
 
 @app.route('/payments/<transaction_id>', methods = ["PUT"])
+@auth.login_required
 def updatePayment(transaction_id):
 	data = request.get_json()							# extract the request body and store it in variable "data"
 	for payment in payments:							# loop over the list 
@@ -88,6 +108,7 @@ def updatePayment(transaction_id):
 
 
 @app.route('/payments/<transaction_id>', methods = ["DELETE"])
+@auth.login_required
 def deletePayment(transaction_id):
 	for index, payment in enumerate(payments):			# loop over the list 
 		if payment["transaction_id"] == transaction_id:	# check transaction_id of each item in the list
@@ -100,11 +121,29 @@ def deletePayment(transaction_id):
 
 @app.route('/signUp', methods=["POST"])
 def signUp():
-	data = request.get_json()				# extract the request body and store it in variable "data"
-	my_users.append(data)					# Add the request body in our "payments" database
+	data = request.get_json()				
+	request_user_name= data["user_name"]
+	request_password = data["password"]
+	hashed_password = generate_password_hash(request_password)
+	
+	users_database[request_user_name] = hashed_password
+	
+	print(users_database)				
  
-	return data						# respond back to client with request body along with newly added fields like transaction ID, timestamp, etc
+	return {"message":"User created"}						
+
+@app.route('/random', methods=["GET"])
+def random_path():
+	number = random.randint(1, 10)
+	if number < 3:
+		return jsonify({"message": "Sending 201"}),201
+	if number == 4:
+		return jsonify({"message": "Sending 201"}),403
+	if number >= 5:
+		return jsonify({"message": "Sending 201"}),500
+	else:
+		return jsonify({"message": "Sending 201"}),204	
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port= 5000, debug=True, ssl_context=('./cert.pem', './key.pem'))
+	app.run(host="0.0.0.0", port= 5000, ssl_context=('server-chain.crt', 'server.key'))
