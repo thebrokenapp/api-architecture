fc08136c87b89fbb5a1313240e9ac82ba5a0e7fc
Ankit Sahay <139492522+thebrokenapp@users.noreply.github.com>
2024-12-06 18:10:53 +0530

    Create dml.sql

---
+++ event_driven/dml.sql
@@ -0,0 +1,12 @@
+INSERT INTO posts (post_id, likes, comments, timestamps, popularity_score) VALUES ('post1', 10, 5, '2024-12-01 10:00:00', 15.0);
+INSERT INTO posts (post_id, likes, comments, timestamps, popularity_score) VALUES ('post2', 20, 8, '2024-12-01 11:00:00', 25.0);
+INSERT INTO posts (post_id, likes, comments, timestamps, popularity_score) VALUES ('post3', 5, 2, '2024-12-01 12:00:00', 7.0);
+INSERT INTO posts (post_id, likes, comments, timestamps, popularity_score) VALUES ('post4', 15, 10, '2024-12-01 13:00:00', 20.0);
+INSERT INTO posts (post_id, likes, comments, timestamps, popularity_score) VALUES ('post5', 8, 3, '2024-12-01 14:00:00', 11.0);
+
+
+INSERT INTO interactions (post_id, interaction_type, interaction_by, timestamp) VALUES ('post1', 'like', 'user1', '2024-12-01 15:00:00');
+INSERT INTO interactions (post_id, interaction_type, interaction_by, timestamp) VALUES ('post2', 'like', 'user2', '2024-12-01 15:10:00');
+INSERT INTO interactions (post_id, interaction_type, interaction_by, timestamp) VALUES ('post3', 'comment', 'user3', '2024-12-01 15:20:00');
+INSERT INTO interactions (post_id, interaction_type, interaction_by, timestamp) VALUES ('post4', 'like', 'user4', '2024-12-01 15:30:00');
+INSERT INTO interactions (post_id, interaction_type, interaction_by, timestamp) VALUES ('post5', 'like', 'user5', '2024-12-01 15:40:00');
