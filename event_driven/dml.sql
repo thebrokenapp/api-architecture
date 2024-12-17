3fe015b9b69de1de7eaf4d89db18dfbec38e0bfe
Ankit Sahay <139492522+thebrokenapp@users.noreply.github.com>
2024-12-17 10:16:08 +0530

    Update dml.sql

--- event_driven/dml.sql
+++ event_driven/dml.sql
@@ -3,8 +3,8 @@ CREATE TABLE posts (post_id TEXT, genre TEXT, likes INTEGER, comments INTEGER, t
 
 
 
-INSERT INTO posts (post_id, likes, comments, timestamps, popularity_score) VALUES ('post1','books', 10, 5, '2024-12-01 10:00:00', 15.0);
-INSERT INTO posts (post_id, likes, comments, timestamps, popularity_score) VALUES ('post2','fashion', 20, 8, '2024-12-01 11:00:00', 25.0);
-INSERT INTO posts (post_id, likes, comments, timestamps, popularity_score) VALUES ('post3','jobs', 5, 2, '2024-12-01 12:00:00', 7.0);
-INSERT INTO posts (post_id, likes, comments, timestamps, popularity_score) VALUES ('post4','tech', 15, 10, '2024-12-01 13:00:00', 20.0);
-INSERT INTO posts (post_id, likes, comments, timestamps, popularity_score) VALUES ('post5','books', 8, 3, '2024-12-01 14:00:00', 11.0);
+INSERT INTO posts (post_id,genre, likes, comments, timestamps, popularity_score) VALUES ('post1','books', 10, 5, '2024-12-01 10:00:00', 15.0);
+INSERT INTO posts (post_id,genre, likes, comments, timestamps, popularity_score) VALUES ('post2','fashion', 20, 8, '2024-12-01 11:00:00', 25.0);
+INSERT INTO posts (post_id,genre, likes, comments, timestamps, popularity_score) VALUES ('post3','jobs', 5, 2, '2024-12-01 12:00:00', 7.0);
+INSERT INTO posts (post_id,genre, likes, comments, timestamps, popularity_score) VALUES ('post4','tech', 15, 10, '2024-12-01 13:00:00', 20.0);
+INSERT INTO posts (post_id,genre, likes, comments, timestamps, popularity_score) VALUES ('post5','books', 8, 3, '2024-12-01 14:00:00', 11.0);
