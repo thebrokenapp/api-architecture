8040342149020f83c60af72f51e6b5d2ac721ce0
Ankit Sahay <139492522+thebrokenapp@users.noreply.github.com>
2024-12-20 00:10:29 +0530

    Update payments.py

--- securing-apis/basic-auth-with-hashing/payments.py
+++ securing-apis/basic-auth-with-hashing/payments.py
@@ -57,8 +57,16 @@ users = {}
 
 @auth.verify_password
 def verify_password(username, password):
-    if username in users and check_password_hash(users.get(username), password):
-        return username
+	conn = get_db_connection()
+	cursor = conn.cursor()
+	cursor.execute('SELECT hashed_password FROM users WHERE user_name = ?', (username,))
+	password_row = cursor.fetchone()
+	retrieved_password = password_row["hashed_password"]
+	print("password from sqlite3:",retrieved_password)
+	print("password from Auth header", password)
+	conn.close()
+	if check_password_hash(retrieved_password, password):
+		return username
 
 
 
@@ -88,11 +96,14 @@ def sign_up():
 	request_user_name= data["user_name"]
 	request_password = data["password"]
 	hashed_password = generate_password_hash(request_password)
-	
-	users[request_user_name] = hashed_password
-	
-	print(users)				
- 
+	print(request_user_name, hashed_password)
+
+	conn = get_db_connection()	# use the function defined above to get a connection to DB
+	cursor = conn.cursor()		# # Creates a cursor object to interact with the database.
+	cursor.execute('''INSERT INTO users (user_name, hashed_password) VALUES (?, ?)''',(request_user_name, hashed_password))
+	conn.commit()
+	conn.close()
+	#users[request_user_name] = hashed_password
 	return {"message":"User created"}
 
 
