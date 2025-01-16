f31eaf91434a8647f174301c017b10cda6057c39
Ankit Sahay <139492522+thebrokenapp@users.noreply.github.com>
2025-01-16 18:14:38 +0530

    Update users.py

--- microservices/users.py
+++ microservices/users.py
@@ -1,39 +1,86 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from datetime import datetime
 import uuid
-from flask import Response
+from pydantic import BaseModel, Field, UUID4
+from flask_pydantic import validate
+import sqlite3
+import redis
+
+
 
 app = Flask(__name__)
 
-@app.route('/user/apiStatus')
-def status():
-	return {"message": "User API is running!"}
+r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
-users = []
 
-@app.route('/user')
-def getUsers():
-	return {"users": users}
+# Function to connect to SQLite database
+def get_db_connection():
+    conn = sqlite3.connect('upi.db')	# This opens the connection to the database.
+    conn.row_factory = sqlite3.Row  # Allow fetching rows as dictionaries
+    return conn
 
-@app.route('/user', methods=["POST"])
-def initiatePayment():
+@app.route('/users', methods=['POST'])
+def create_user():
 	data = request.get_json()				# extract the request body and store it in variable "data"
-	timestamp = datetime.utcnow()			# create a new timestamp to capture the transaction time
-	data["status"] = "initiated"			# by default all transactions starts with status as "initiated"
-	data["timestamp"] = timestamp 			# Attach timestamp in the request body
-	users.append(data)					# Add the request body in our "payments" database
- 
-	return data								# respond back to client with request body along with newly added fields like transaction ID, timestamp, etc
+	user_name = data.get("user_name")		# extract user name from request body
+	password = data.get("password")			# extract password from request body
+	registration_date = datetime.utcnow()
+	product = data.get("product")
+	
+	conn = get_db_connection()	# use the function defined above to get a connection to DB
+	cursor = conn.cursor()		# # Creates a cursor object to interact with the database.
+	cursor.execute('''INSERT INTO users (user_name, password, registration_date, product) VALUES (?, ?, ?,?)''',
+	(user_name, password, registration_date, product))
+	conn.commit()
+	conn.close()
+	return {"message": "User created"},201
 
 
-@app.route('/user/<user_id>')
-def getUser(user_id):
-	for user in users:				# loop over the list 
-		if user["user_id"] == user_id:	# check transaction_id of each item in the list
-			return user                              # if anything matches, then return the item
+@app.route('/users', methods=['GET'])
+def get_all_users():
+	conn = get_db_connection()	# use the function defined above to get a connection to DB
+	cursor = conn.cursor()		# # Creates a cursor object to interact with the database.
+	cursor.execute('''SELECT * FROM users''')
+	users = cursor.fetchall()
+	users_list = []
+	for row in users:
+		users_list.append( {"user_name": row[0], "password": row[1], "date": row[2], "product": row[3]})
+	conn.close()
+	return jsonify({"users": users_list}),200
 
-	return jsonify({"message": "User not found"}),404   # otherwise return 404 
+
+@app.route('/users/<user_name>', methods=['GET'])
+def get_one_user(user_name):
+	conn = get_db_connection()	# use the function defined above to get a connection to DB
+	cursor = conn.cursor()		# # Creates a cursor object to interact with the database.
+	cursor.execute('''SELECT * FROM users where user_name = ?''',(user_name,))
+	user = cursor.fetchone()
+
+	if user is None:
+		conn.close()
+		return {"message": "User not found"}, 404
+
+	user_data = {"user_name": user[0], "password": user[1], "date": user[2], "product": user[3]}
+	conn.close()
+	return jsonify(user_data), 200
+
+
+
+@app.route('/users/<user_name>', methods=['DELETE'])
+def delete_one_user(user_name):
+	conn = get_db_connection()	# use the function defined above to get a connection to DB
+	cursor = conn.cursor()		# # Creates a cursor object to interact with the database.
+	cursor.execute("""DELETE FROM users WHERE user_name = ?""", (user_name,))
+	conn.commit()
+
+	if cursor.rowcount == 0:  # Check if no rows were deleted
+		conn.close()
+		return {"message": "User not found"}, 404
+
+	conn.close()
+	return {}, 204
+
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port= 8001, debug=True)
+	app.run(host="0.0.0.0", port= 8002, debug=True)
