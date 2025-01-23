1df58c09f629b4c581834f2998a634559dc4068b
Ankit Sahay <139492522+thebrokenapp@users.noreply.github.com>
2025-01-23 23:08:40 +0530

    Create fraud_detection.py

---
+++ event_driven/payments/fraud_detection.py
@@ -0,0 +1,27 @@
+import redis
+import json
+import random
+
+# Connect to Redis
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+
+# Subscribe to the payment channel
+pubsub = redis_client.pubsub()
+pubsub.subscribe("initiate_payment_channel")
+
+print("Listening for payment messages...")
+
+# Listen for messages and process them
+for message in pubsub.listen():
+    if message['type'] == 'message':
+        # Parse the message data
+        payment_data = json.loads(message['data'])
+        print(payment_data)
+        # Randomly decide if it's fraud or not
+        if random.choice([True, False]):
+            fraud_status = "fraud"
+        else:
+            fraud_status = "not fraud"
+        
+        # Print the result
+        print("Payment ID:", payment_data['transaction_id'], " Status:", fraud_status)
